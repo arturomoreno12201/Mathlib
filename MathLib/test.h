af775f2bc16166f9eed344f0c3396ac3b1bb6c3e
arturo.moreno <arturomoreno151@gmail.com>
2016-09-26 15:45:54 -0700

    math

--- MathLib/test.h
+++ MathLib/test.h
@@ -1,5 +1,36 @@
 #pragma once
 
+int doNothing(int x);
+
+// Problem a:
+float test_quad(float x);
+
+//Problem b:
 
 
-int something(int  x);
+struct quad_results
+{
+	float left_root, right_root;
+	int roots; //0, 1, 2
+};
+
+quad_results quad(float a, float b, float c);
+
+
+// problem b.
+float lerp(float start, float end, float t);
+
+// problem c.
+struct Point { float x, y; };
+float distance(const Point &P1, const Point &P2);
+
+// problem d.
+struct Point3D { float x, y, z; };
+float inner(const Point3D &P1, const Point3D &P2);
+
+struct Plane { Point3D abc; float d; };
+float point_plane_distance(const Plane &pl, const Point3D &pt);
+
+Point3D bezier(float t, const Point3D &p1, const Point3D &p2,
+	const Point3D &p3, const Point3D &p4);
+
--- MathTest/main.cpp
+++ MathTest/main.cpp
@@ -1,15 +1,52 @@
-#include<cassert>
-#include<iostream>
-#include "test.h"
+#include <cassert>
+#include <cstdio>
+#include "Test.h"
 
 
-
-int main() 
+int main()
 {
+	printf("Testing doNothing\n");
+	assert(doNothing(0) == 5);
+	assert(doNothing(-1) == 4);
+	assert(doNothing(1) == 8);
 
-	printf("hi mom %d", something(100));
+	assert(test_quad(0) == -7);
+	assert(test_quad(-1) == -8);
+	assert(test_quad(1) == -4);
 
+	// Testing Quadratic Function...
+	assert(quad(1, 0, 0).roots == 1);
+	assert(quad(1, -2, 0).roots == 2);
+	assert(quad(1, 0, 4).roots == 0);
 
-	getchar();
+	assert(quad(1, -2, 0).left_root == 0);
+	assert(quad(1, -2, 0).right_root == 2);
+	assert(quad(1, 0, 0).left_root == quad(1, 0, 0).right_root);
+
+	// Testing Linear Interpolation
+	assert(lerp(0, 1, 0) == 0);
+	assert(lerp(0, 1, 1) == 1);
+	assert(lerp(0, 1, -1) == -1);
+	assert(lerp(1, -1, .5f) == 0);
+
+	// Testing Distance formula
+	assert(distance({ 0, 0 }, { 0,0 }) == 0);
+	assert(distance({ 0, 0 }, { 5,0 }) == 5);
+	assert(distance({ 0,-5 }, { 0,0 }) == 5);
+	assert(distance({ 3,0 }, { 0,4 }) == 5);
+
+	// Inner Product
+	assert(inner({ 1,1,0 }, { -1,1,0 }) == 0);
+	assert(inner({ 1,1,1 }, { 1,1,1 }) == 3);
+
+	// Point-Plane Distance
+	assert(point_plane_distance({ 0,0,1,0 }, { 0,0,0 }) == 0);
+	assert(point_plane_distance({ 0,0,1,0 }, { 0,0,1 }) == 1);
+
+	// Bezier
+	assert(bezier(0, { 0,0,0 }, { 1,0,0 }, { 2,0,0 }, { 3,0,0 }).x == 0);
+	assert(bezier(1, { 0,0,0 }, { 1,0,0 }, { 2,0,0 }, { 3,0,0 }).x == 3);
+	assert(bezier(0.5f, { 0,0,0 }, { 1,0,0 }, { 2,0,0 }, { 3,0,0 }).x == 1.5);
+
 	return 0;
 }
